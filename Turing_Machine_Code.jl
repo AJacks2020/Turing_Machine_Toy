9712dc30bb64fae02faf6cc25bfdd6bc3889e1f9
Andrew Jackson <72963642+AJacks2020@users.noreply.github.com>
2023-12-16 15:07:05 +0000

    Update Turing_Machine_Code.jl
    Changed all UInt8 to Int8. 
    
    Warning!
    Code not tested post change.

--- Turing_Machine_Code.jl
+++ Turing_Machine_Code.jl
@@ -8,27 +8,27 @@ mutable struct turingMachine
     # The string of symbols on the worktape of the Turing machine, also where the initial input it given
     tape :: String
     # The index of the character in the tape string that the Turing machine's head (what is uses to read/write the tape) is over/can act on
-    headLocation :: UInt8
+    headLocation :: Int8
     # The label of the internal state of the Turing machine
-    machineState :: UInt8
+    machineState :: Int8
     # The transition table defining the transition table - and hence the dynamics - of the Turing machine
-    transitionTable :: Dict{Tuple{Char, UInt8}, Tuple{Char, UInt8, UInt8}}
+    transitionTable :: Dict{Tuple{Char, Int8}, Tuple{Char, Int8, Int8}}
     # Sets containing the accepting and rejecting states, respectively. I.e. the states where the machine halts
-    acceptingStates :: Set{UInt8}
-    rejectingStates :: Set{UInt8}
+    acceptingStates :: Set{Int8}
+    rejectingStates :: Set{Int8}
 end
 
-function initialize_machine(name::String, input::String, instructions::Dict{Tuple{Char, UInt8}, Tuple{Char, UInt8, UInt8}}, accepting::Set{UInt8},
-    rejecting::Set{UInt8})
+function initialize_machine(name::String, input::String, instructions::Dict{Tuple{Char, Int8}, Tuple{Char, Int8, Int8}}, accepting::Set{Int8},
+    rejecting::Set{Int8})
     #=
         A simple function to initilize a turingMachine object with a valid initial setup
 
         INPUTS
             name         : The name to be given to the created Turing machine                       : String
             input        : The string of symbols that are initially on the Turing machine's tape    : String
-            instructions : The transition table defining the Turing machine                         : Dict{Tuple{Char, UInt8}, Tuple{Char, UInt8, UInt8}}
-            accepting    : The states of the Turing machine that indicate it should halt and accept : Set{UInt8}
-            rejecting    : The states of the Turing machine that indicate it should halt and reject : Set{UInt8}
+            instructions : The transition table defining the Turing machine                         : Dict{Tuple{Char, Int8}, Tuple{Char, Int8, Int8}}
+            accepting    : The states of the Turing machine that indicate it should halt and accept : Set{Int8}
+            rejecting    : The states of the Turing machine that indicate it should halt and reject : Set{Int8}
 
         OUTPUT
             A turingMachine object with the specified properties, in a valid initial setup
@@ -52,7 +52,7 @@ function singleStep(turingMachine)
 
     # Finds the changes to make to the Turing machine based on the current state
     char_under_head::Char = turingMachine.tape[turingMachine.headLocation]
-    changes_to_make::Tuple{Char, UInt8, UInt8} = getindex(turingMachine.transitionTable, (char_under_head, turingMachine.machineState))
+    changes_to_make::Tuple{Char, Int8, Int8} = getindex(turingMachine.transitionTable, (char_under_head, turingMachine.machineState))
 
     # Constructs the updated string of the tape
     new_tape::Vector{Char} = collect.(turingMachine.tape)
@@ -102,16 +102,16 @@ testInput::String = "11000110111" # CHANGE THIS HOWEVER YOU LIKE
 
 
 testName::String = "Bob"
-setAccepting::Set{UInt8} = Set(convert(Int8, 4))
-setRejecting::Set{UInt8} = Set(convert(Int8, 3))
+setAccepting::Set{Int8} = Set(convert(Int8, 4))
+setRejecting::Set{Int8} = Set(convert(Int8, 3))
 
 # Only use one of the below transition tables at a time: it is what specifies what the Turing Machine does. Also need to change the accpeting and rejecting states
 
 # Decides if the first bit of a binary string is '1'
-#transitionTable::Dict{Tuple{Char, UInt8}, Tuple{Char, UInt8, UInt8}}  = Dict(('0', 0 ) => ('0', 0, 1), ('1', 0 ) => ('1', 0, 2))
+#transitionTable::Dict{Tuple{Char, Int8}, Tuple{Char, Int8, Int8}}  = Dict(('0', 0 ) => ('0', 0, 1), ('1', 0 ) => ('1', 0, 2))
 
 # Decides if the third bit of a binary string is '1'
-transitionTable::Dict{Tuple{Char, UInt8}, Tuple{Char, UInt8, UInt8}}  = Dict(('0', 0 ) => ('0', 1, 1), ('1', 0 ) => ('1', 1, 1), ('0', 1 ) => ('0', 1, 2), ('1', 1 ) => ('1', 1, 2), ('0', 2 ) => ('0', 0, 3), ('1', 2 ) => ('1', 0, 4)) 
+transitionTable::Dict{Tuple{Char, Int8}, Tuple{Char, Int8, Int8}}  = Dict(('0', 0 ) => ('0', 1, 1), ('1', 0 ) => ('1', 1, 1), ('0', 1 ) => ('0', 1, 2), ('1', 1 ) => ('1', 1, 2), ('0', 2 ) => ('0', 0, 3), ('1', 2 ) => ('1', 0, 4)) 
 
 testingMachine = initialize_machine(testName, testInput, transitionTable, setAccepting, setRejecting)
 testOutput::Bool = simulationMachine(testingMachine)
